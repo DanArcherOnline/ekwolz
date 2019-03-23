5b525573b6a89343cb27eebf27651838e7a60ef6
DanArcherOnline <danarcheronline@gmail.com>
2019-03-23 22:36:52 +0900

    Feat: Add support so that the nav and status bar can be transparent
    The nav and status is set to be transparent. Therefore the window
    has been set to take up the size of the screen. The content of the
    app starts from the bottom of the screen which overlaps with the
    navigation bar, so support was add to:
    1) get the size of the navigation bar
    2) detect where the navigation bar is depending on the orientation
    and device type (its always at the bottom on tablets)
    3) add nav bars height to the side of where the nav bar is located

--- app/src/main/java/com/danarcheronline/ekwolz/MainActivity.java
+++ app/src/main/java/com/danarcheronline/ekwolz/MainActivity.java
@@ -1,13 +1,61 @@
 package com.danarcheronline.ekwolz;
 
+import android.content.Context;
+import android.hardware.display.DisplayManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    DisplayManager.DisplayListener mDisplayListener;
+    int mAppContainerResourceId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppContainerResourceId = R.id.appContainer;
+
+//        make the status bar and navigation bar transparent
+        UIUtils.setSystemUIBarsToTransparent(this, mAppContainerResourceId);
+
+        enableMarginAdjustmentDetection();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        DisplayManager displayManager = (DisplayManager) this.getSystemService(Context.DISPLAY_SERVICE);
+        displayManager.unregisterDisplayListener(mDisplayListener);
+
+    }
+
+    private void enableMarginAdjustmentDetection() {
+        mDisplayListener = new DisplayManager.DisplayListener() {
+
+            @Override
+            public void onDisplayAdded(int displayId) {
+            }
+
+            @Override
+            public void onDisplayRemoved(int displayId) {
+            }
+
+            @Override
+            public void onDisplayChanged(int displayId) {
+                UIUtils.adjustOrientationMargins(MainActivity.this, mAppContainerResourceId);
+            }
+        };
+
+        DisplayManager displayManager = (DisplayManager) this.getSystemService(Context.DISPLAY_SERVICE);
+        displayManager.registerDisplayListener(mDisplayListener, new Handler());
     }
 }
+
+
